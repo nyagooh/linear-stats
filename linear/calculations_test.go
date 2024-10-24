a1743bf381a92f0534aab7d0bada8e631ed849bc
nymaina <annmaina614@gmail.com>
2024-10-24 11:57:04 +0300

    Add more test

--- linear/calculations_test.go
+++ linear/calculations_test.go
@@ -11,27 +11,24 @@ func TestLinearRegression(t *testing.T) {
 		expectedB float64
 	}{
 		{
-			values:    []float64{189,113,121,114,145,110},
-			expectedM: -8.742857,
-			expectedB: 153.857143,
+			values:    []float64{189, 113, 121, 114, 145, 110},
+			expectedM: -8.742857142857142,
+			expectedB: 153.85714285714286,
 		},
 		{
-			values:    []float64{189.8,113.6,121.7,114.9,145.1,110},
-			expectedM:  -8.894286,
-			expectedB: 154.752381,
+			values:    []float64{189.8, 113.6, 121.7, 114.9, 145.1, 110},
+			expectedM: -8.894285714285727,
+			expectedB: 154.752380952381,
 		},
 		{
-			values:    []float64{-189,113,121,-114,-145,110},
-			expectedM: 13.885714,
-			expectedB: -52.047619,
+			values:    []float64{-189, 113, 121, -114, -145, 110},
+			expectedM: 13.885714285714286,
+			expectedB: -52.047619047619044,
 		},
-	
-	
 	}
 
 	for _, test := range tests {
 		m, b := LinearRegression(test.values)
-
 		if m != test.expectedM || b != test.expectedB {
 			t.Errorf("LinearRegression(%v) = (%.6f, %.6f), want (%.6f, %.6f)", test.values, m, b, test.expectedM, test.expectedB)
 		}
@@ -44,25 +41,24 @@ func TestCalculatePearson(t *testing.T) {
 		expectedM float64
 	}{
 		{
-			values:    []float64{189.8,113.6,121.7,114.9,145.1,110},
-			expectedM:  -0.5408937201,
+			values:    []float64{189.8, 113.6, 121.7, 114.9, 145.1, 110},
+			expectedM: -0.5408937201329931,
 		},
 		{
-			values:    []float64{189,113,121,114,145,110},
-			expectedM: -0.5330331012,
-			
+			values:    []float64{189, 113, 121, 114, 145, 110},
+			expectedM: -0.5330331011560401,
 		},
 		{
-			values:    []float64{-189,113,121,-114,-145,110},
-			expectedM:  0.1772088416,
+			values:    []float64{-189, 113, 121, -114, -145, 110},
+			expectedM: 0.1772088415931945,
 		},
 	}
 
 	for _, test := range tests {
 		m := CalculatePearson(test.values)
 
-		if m != test.expectedM  {
-			t.Errorf("CalculatePearson(%v) = (%.6f), want (%.6f)", test.values, m,test.expectedM)
+		if m != test.expectedM {
+			t.Errorf("CalculatePearson(%v) = (%.6f), want (%.6f)", test.values, m, test.expectedM)
 		}
 	}
 }
