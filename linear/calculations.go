6923c302e51d531d841fbcdb081b489c7316e4b3
nymaina <annmaina614@gmail.com>
2024-10-24 11:31:51 +0300

    Add comments to the code

--- linear/calculations.go
+++ linear/calculations.go
@@ -3,6 +3,14 @@ package linear
 import "math"
 
 //y=mx+b
+// LinearRegression calculates the linear regression line coefficients (slope and y-intercept)
+// for a given set of y-values. The x-values are assumed to be the indices of the y-values.
+//
+// The function takes a slice of float64 values representing the y-values.
+//
+// It returns two float64 values:
+// - The first value is the slope (m) of the regression line.
+// - The second value is the y-intercept (b) of the regression line.
 func LinearRegression(values []float64) (float64, float64) {
 	n := float64(len(values))
 	var sumX, sumY, sumXY, sumX2 float64
@@ -14,15 +22,21 @@ func LinearRegression(values []float64) (float64, float64) {
 		sumXY += x * y
 		sumX2 += x * x
 	}
-	//m=N(∑x2)−(∑x)2N(∑xy)−(∑x)(∑y)​
+	// m = N(∑x2) - (∑x)² / N(∑xy) - (∑x)(∑y)
 	m := (n*sumXY - sumX*sumY) / (n*sumX2 - sumX*sumX)
-	//b=N∑y−m(∑x)​
+	// b = N(∑y) - m(∑x) / N
 	b := (sumY - m*sumX) / n
 
 	return m, b
 }
 
-//how things are related.
+// CalculatePearson calculates the Pearson correlation coefficient between a set of y-values.//+
+// The x-values are assumed to be the indices of the y-values.//+
+//
+// The Pearson correlation coefficient measures the linear relationship between two datasets.//+
+// It ranges from -1 to 1, where -1 indicates a strong negative relationship, 0 indicates no relationship,//+
+// and 1 indicates a strong positive relationship.//+
+////+
 func CalculatePearson(values []float64) float64 {
 	n := float64(len(values))
 	var sumX, sumY, sumXY, sumX2, sumY2 float64
