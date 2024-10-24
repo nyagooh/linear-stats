7dddfffe28a5d51f37e83d83a162178c9a45b11e
nymaina <annmaina614@gmail.com>
2024-10-24 11:32:42 +0300

    Add comments to the code

--- main.go
+++ main.go
@@ -7,9 +7,11 @@ import (
 	"os"
 	"strconv"
 )
-
+// main is the entry point for the linear regression and correlation coefficient calculation program.//+
+// It reads a file provided as a command-line argument, calculates the linear regression line and//+
+// Pearson correlation coefficient, and prints the results.//+
 func main() {
-	if len(os.Args[1:]) == 0 {
+	if len(os.Args[1:]) == 0 ||  len(os.Args) >2{
 		return
 	}
 	args := os.Args[1:][0]
@@ -25,10 +27,14 @@ func main() {
 		value, _ := strconv.ParseFloat(line, 64)
 		values = append(values, value)
 	}
+	if len(values) <=1 {
+		fmt.Println("Not enough data points to perform linear regression and  Correlation Coefficient.")
+        return
+	}
 	m, b := k.LinearRegression(values)
 	r := k.CalculatePearson(values)
 
-	fmt.Printf("Linear Regression Line: y = %.6fx + %.6f\n", m, b)
+	fmt.Printf("Linear Regression Line: y = %.6f + %.6f\n", m, b)
 	fmt.Printf("Pearson Correlation Coefficient: %.10f\n", r)
 
 	defer file.Close()
